f6291ade4d2415d8feefdd2e952e7c905f3f599b
Tommy Inouye <ti2181@columbia.edu>
2014-03-24 17:54:43 -0400

    made some changes

--- parseTester.py
+++ parseTester.py
@@ -4,13 +4,44 @@ from MAPlexer import *
 
 #Sandya, put any input you want to test here
 ###############################################
-test='100000 read()'
+test=''
 lexer=lex.lex()
 lexer.input(test)
 
 for tok in lexer:
 	print tok.type, tok.value
 ###############################################
+
+def testlex():
+	#tester to make sure that the tokens work
+	
+	#check numeric
+	test='10 10.5 10.9 1000000 2138920183 102.202 .02931'
+	lexer=lex.lex()
+	lexer.input(test)
+	for tok in lexer:
+		if tok.type!='NUMERIC':
+			print "Test1 failed"
+			return False
+	print "Test1 passed"
+
+	test='""""""""""""""'
+	lexer=lex.lex()
+	lexer.input(test)
+	for tok in lexer:
+		if tok.type!='DOUBLEQUOTE':
+			print "Test2 failed"
+			return False
+
+	print "Test2 passed"
+
+
+
+
+
+
+testlex()
+
 
 
 
