9cabbb68e96e344a13cb4842041385c5f52f0037
Alex M <mereck@gmail.com>
2014-03-28 14:01:04 -0400

    whatevs

--- yacc.py
+++ yacc.py
@@ -1,3 +1,18 @@
+'''ERROR: yacc.py:71: Symbol 'IN' used, but not defined as a token or a rule
+ERROR: yacc.py:83: Symbol 'ASSIGN' used, but not defined as a token or a rule
+ERROR: yacc.py:118: Symbol 'multiplicative-exression' used, but not defined as a token or a rule
+ERROR: yacc.py:129: Symbol 'DIVIDES' used, but not defined as a token or a rule
+ERROR: yacc.py:133: Symbol 'constant' used, but not defined as a token or a rule
+ERROR: yacc.py:134: Symbol 'string' used, but not defined as a token or a rule
+ERROR: yacc.py:135: Symbol 'node' used, but not defined as a token or a rule
+ERROR: yacc.py:136: Symbol 'edge' used, but not defined as a token or a rule
+ERROR: yacc.py:141: Symbol 'DOT' used, but not defined as a token or a rule
+ERROR: yacc.py:144: Symbol 'print' used, but not defined as a token or a rule
+ERROR: yacc.py:145: Symbol 'read' used, but not defined as a token or a rule
+ERROR: yacc.py:146: Symbol 'write' used, but not defined as a token or a rule
+'''
+
+
 import ply.yacc as yacc
 from MAPlexer import * 
 #parsing rules
@@ -80,7 +95,7 @@ def p_aexpr(t):
 	'assignment-expression : conditional-expression'
 
 def p_aexpr2(t):
-	'assignment-expression : primary-expression ASSIGN assignment-expression'
+	'assignment-expression : primary-expression EQUALS assignment-expression'
 
 def p_condexpr(t):
 	'''conditional-expression : logical-OR-expression
@@ -115,7 +130,7 @@ def p_relexpr2(t):
 	| relational-expression GREATERTHANOREQUALTO additive-expression'''
 
 def p_addexpr(t):
-	'additive-expression : multiplicative-exression'
+	'additive-expression : multiplicative-expression'
 
 def p_addexpr2(t):
 	'''additive-expression : additive-expression PLUS multiplicative-expression
@@ -126,7 +141,7 @@ def p_multexpr(t):
 
 def p_multexpr2(t):
 	'''multiplicative-expression : multiplicative-expression TIMES primary-expression
-	| multiplicative-expression DIVIDES primary-expression'''
+	| multiplicative-expression DIVIDE primary-expression'''
 
 def p_primexp(t):
 	'''primary-expression : identifier
