644082a4420e150e57dedf8c1ae4b4947d108cca
Alex M <mereck@gmail.com>
2014-03-30 18:22:22 -0400

    working on hello world parse

--- yacc.py
+++ yacc.py
@@ -1,42 +1,8 @@
-'''ERROR: yacc.py:71: Symbol 'IN' used, but not defined as a token or a rule
-ERROR: yacc.py:83: Symbol 'ASSIGN' used, but not defined as a token or a rule
-ERROR: yacc.py:118: Symbol 'multiplicative-exression' used, but not defined as a token or a rule
-ERROR: yacc.py:129: Symbol 'DIVIDES' used, but not defined as a token or a rule
-ERROR: yacc.py:133: Symbol 'constant' used, but not defined as a token or a rule
-ERROR: yacc.py:134: Symbol 'string' used, but not defined as a token or a rule
-ERROR: yacc.py:135: Symbol 'node' used, but not defined as a token or a rule
-ERROR: yacc.py:136: Symbol 'edge' used, but not defined as a token or a rule
-ERROR: yacc.py:141: Symbol 'DOT' used, but not defined as a token or a rule
-ERROR: yacc.py:144: Symbol 'print' used, but not defined as a token or a rule
-ERROR: yacc.py:145: Symbol 'read' used, but not defined as a token or a rule
-ERROR: yacc.py:146: Symbol 'write' used, but not defined as a token or a rule
-'''
-
-
 import ply.yacc as yacc
 from MAPlexer import * 
-#parsing rules
 
-# Parsing rules
-
-'''def p_tu_ed(t):
-	'translation-unit : external-declaration'
-	t[0] = t[1]
-
-def p_tu_ed2(t):
-	'translation-unit : translation-unit external-declaration'
-	t[0] = t[1] + ' ' + t[2]
-
-def p_ed1(t):
-	'external-declaration : function-definition'
-	t[0] = t[1]
-
-def p_ed2(t):
-	'external-declaration : statement'
-	t[0] = t[1]
-'''
 def p_fd(t):
-	'function-definition : FUNC identifier LPAREN parameter-list RPAREN' # compound-statement'
+	'function-definition : FUNC identifier LPAREN parameter-list RPAREN compound-statement'
 	print "funtion-definition : {1} {2}{3}{4}{5}".format(t[0],t[1],t[2],t[3],t[4],t[5])
 	t[0] = t[1] + t[2] + t[3] + t[4] + t[5]
 
@@ -58,6 +24,11 @@ def p_plist2(t):
 
 def p_cs(t):
 	'compound-statement : LBR statement-list RBR'
+	t[0] = t[1] + t[2] + t[3]
+
+def p_cs_E(t):
+	'compound-statement : '
+	t[0] = ""
 
 def p_slist(t):
 	'statement-list : statement'
@@ -65,10 +36,12 @@ def p_slist(t):
 
 def p_slist2(t):
 	'statement-list : statement-list statement'
+	t[0] = t[1] + t[2]
 
 def p_slist3(t):
 	'statement-list : '
 	t[0] = ""
+
 def p_s(t):
 	'''statement : expression
 	| compound-statement
@@ -78,88 +51,107 @@ def p_s(t):
 
 def p_sels(t):
 	'selection-statement : IF LPAREN expression RPAREN statement'
+	t[0] = t[1] + t[2] + t[3] + t[4] + t[5]
 
 def p_sels2(t):
 	'selection-statement : IF LPAREN expression RPAREN statement ELSE statement'
-
+	t[0] = t[1] + t[2] + t[3] + t[4] + t[5] + t[6] + t[7]
 def p_iters(t):
 	'iteration-statement : FOR LPAREN expression SEMICOLON expression SEMICOLON expression RPAREN statement'
-
+	t[0] = t[1] + t[2] + t[3] + t[4] + t[5] + t[6] + t[7] + t[8] + t[9]
 def p_ters2(t):
 	'iteration-statement : FOREACH LPAREN identifier IN identifier RPAREN statement'
-
+	t[0] = t[1] + t[2] + t[3] + t[4] + t[5] + t[6] + t[7]
 def p_expr(t):
 	'expression : assignment-expression'
+	t[0] = t[1]
 
 def p_expr2(t):
 	'expression : expression COMMA assignment-expression'
-
+	t[0] = t[1] + t[2] + t[3] 
 def p_aexpr(t):
 	'assignment-expression : conditional-expression'
-
+	t[0] = t[1]
 def p_aexpr2(t):
 	'assignment-expression : primary-expression EQUALS assignment-expression'
+	t[0] = t[1] + t[2] + t[3] 
 
 def p_condexpr(t):
 	'''conditional-expression : logical-OR-expression
 	| logical-AND-expression'''
+	t[0] = t[1]
 
 def p_logorexpr(t):
 	'logical-OR-expression : logical-AND-expression'
+	t[0] = t[1]
 
 def p_logorexpr2(t):	
 	'logical-OR-expression : logical-OR-expression LOGICALOR logical-AND-expression'
+	t[0] = t[1] + t[2] + t[3]
 
 def p_logandexpr(t):
 	'logical-AND-expression : equality-expression'
+	t[0] = t[1]
 
 def p_logandexpr2(t):
 	'logical-AND-expression : logical-AND-expression LOGICALAND equality-expression'
+	t[0] = t[1] + t[2] + t[3]
 
 def p_eqexpr(t):
 	'equality-expression : relational-expression'
+	t[0] = t[1]
 
 def p_eqexpr2(t):
 	'''equality-expression : equality-expression EQUALSEQUALS relational-expression
 	| equality-expression DOESNOTEQUAL relational-expression'''
+	t[0] = t[1] + t[2] + t[3]
 
 def p_relexpr(t):
 	'relational-expression : additive-expression'
+	t[0] = t[1]
 
 def p_relexpr2(t):
 	'''relational-expression : relational-expression GREATERTHAN additive-expression
 	| relational-expression LESSTHAN additive-expression
 	| relational-expression LESSTHANOREQUALTO additive-expression
 	| relational-expression GREATERTHANOREQUALTO additive-expression'''
+	t[0] = t[1] + t[2] + t[3]
 
 def p_addexpr(t):
 	'additive-expression : multiplicative-expression'
+	t[0] = t[1]
 
 def p_addexpr2(t):
 	'''additive-expression : additive-expression PLUS multiplicative-expression
 	| additive-expression MINUS multiplicative-expression'''
+	t[0] = t[1] + t[2] + t[3]
 
 def p_multexpr(t):
 	'multiplicative-expression : primary-expression'
+	t[0] = t[1]
 
 def p_multexpr2(t):
 	'''multiplicative-expression : multiplicative-expression TIMES primary-expression
 	| multiplicative-expression DIVIDE primary-expression'''
+	t[0] = t[1] + t[2] + t[3]
 
 def p_primexp(t):
 	'''primary-expression : identifier
 	| TEXT
 	| NODE
-	| LPAREN expression RPAREN'''
-	#| function-call'''
-'''
+	| LPAREN expression RPAREN
+	| function-call'''
+	t[0] = t[1]
+
 def p_funcall(t):
-	'function-call : identifier DOT function-name LPAREN parameter-list RPAREN'
-'''
+	'function-call : identifier PERIOD function-name LPAREN parameter-list RPAREN'
+	t[0] = t[1] + t[2] + t[3] + t[4] + t[5] + t[6]
+
 def p_funcall2(t):
 	'''function-call : PRINT LPAREN identifier RPAREN
 	| READ LPAREN identifier RPAREN
 	| WRITE LPAREN identifier COMMA identifier RPAREN'''
+	t[0] = t[1] + t[2] + t[3] + t[4]
 
 def p_funcname(t):
 	'''function-name : ADD
@@ -171,13 +163,14 @@ def p_funcname(t):
 	| DELETEEDGEFUNC
 	| FINDSHORTESTFUNC
 	| EQUALSFUNC'''
+	t[0] = t[1]
+
 def p_error(t):
-	print("Syntax error at '%s'" % t.value)
+	print("Syntax error at '%s'" % t)
 
 yacc.yacc()
 
 
-print yacc.parse("func main(hi, bye) ")
-		# {\n\tText t = \"Hello, world\";\n\tprint(t);\n}")
+print yacc.parse("func main(hi, bye) { Text t = 'Hello, world'; print(t);}")
 
 
