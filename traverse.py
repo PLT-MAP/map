d72e01ef08d532a8178d2ecb8bd2b23ff58745b6
serenasimkus <sks2187@columbia.edu>
2014-04-07 17:16:53 -0400

    Not sure if any of this will work

--- traverse.py
+++ traverse.py
@@ -6,18 +6,48 @@ class Traverse(object):
 		self.f = file
 		
 		self.relops = {'<','>','<=','>=','==','!=','+','-','*','/','%'}
-	
+		self.future_imports = []
+		self.tempPoints = set()
+
+		# type table for variables
+		self.symbols = {}
+		self.values = {}
+		self._indent = 0
+		self.x = self.dispatch(tree)
+		self.f.write("")
+		self.f.flush()
 
 
 
-
+	def fill(self, text=""):
+		'''Indent a piece of text, according to the current indentation level.'''
+		self.f.write("\n" + "    "*self._indent + text)
 
 	def write(self, text):
 		'''Append the text passed in to the current line.'''
 		self.f.write(text)
 
 	def enter(self):
-		'''Create a new scope associated with the corresponding : and appropriate indentation.'''
+		'''Create a new scope associated with the corresponding : and increase to
+		the appropriate indentation.'''
+		self.write(":")
+		self._indent += 1
+
+	def leave(self):
+		'''Decrease the indentation level.'''
+		self._indent -= 1
+
+	def dispatch(self, tree):
+		'''Dispatcher function, dispatching tree type T to method _T.'''
+		if isinstance(tree, list):
+			for t in tree:
+				self.dispatch(t, flag)
+			return
+		meth = getattr(self, "_"+tree.type)
+		x = meth(tree, flag)
+		return x.
+
+
 
 	def listtoparams(self, l, x=None):
 		s = ""
