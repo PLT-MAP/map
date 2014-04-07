ebb6d22ed2aa24c55ef121cfe6766d2d6758c07e
serenasimkus <sks2187@columbia.edu>
2014-04-07 17:02:37 -0400

    Not sure if any of this will work

--- traverse.py
+++ traverse.py
@@ -19,4 +19,53 @@ class Traverse(object):
 	def enter(self):
 		'''Create a new scope associated with the corresponding : and appropriate indentation.'''
 
+	def listtoparams(self, l, x=None):
+		s = ""
+		comma = False
+		for a in l:
+			if comma:
+				s += ","
+			else:
+				comma = True
+			s += a
+			if x:
+				self.waitingfor.add(a)
+		return s
 
+	def _logical_OR_expression(self, tree, flag=None):
+		if tree.leaf:
+			s = self.dispatch(tree.children[0], flag) + " or " + self.dispatch(tree.children[1], flag)
+			return s
+		return self.dispatch(tree.children[0], flag)
+
+	def _logical_AND_expression(self, tree flag=None):
+		if tree.leaf:
+			s = self.dispatch(tree.children[0], flag) + " and " + self.dispatch(tree.children[1], flag)
+			return s
+		return self.dispatch(tree.children[0], flag):
+
+	def _equality_expression(self, tree, flag=None):
+		if tree.leaf:
+			s = self.dispatch(tree.children[0], flag) + tree.leaf + self.dispatch(tree.children[1], flag)
+			return s
+		return self.dispatch(tree.children[0], flag)
+
+	def _relational_expression(self, tree, flag=None):
+		if tree.leaf:
+			s = self.dispatch(tree.children[0], flag) + tree.leaf + self.dispatch(tree.children[1], flag)
+			return s
+		return self.dispatch(tree.children[0], flag)
+
+	def _additive_expression(self, tree, flag=None):
+		if tree.leaf:
+			s = self.dispatch(tree.children[0], flag) + tree.leaf + self.dispatch(tree.children[1], flag)
+			return s
+		return self.dispatch(tree.children[0], flag)
+
+	def _multiplicative_expression(self, tree, flag=None):
+		if tree.leaf:
+			s = self.dispatch(tree.children[0], flag) + tree.leaf + self.dispatch(tree.children[1], flag)
+			return s
+		return self.dispatch(tree.children[0], flag) 
+
+	
