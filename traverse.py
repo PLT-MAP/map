f4c57ba52ebb2b70f62fe8256af10576fe86fd8a
serenasimkus <sks2187@columbia.edu>
2014-04-07 16:50:20 -0400

    Still not sure how we are going to make this work

--- traverse.py
+++ traverse.py
@@ -3,7 +3,20 @@ import sys
 class Traverse(object):
 	
 	def __init__(self, tree, file=sys.stdout):
-		self.file = file
+		self.f = file
 		
 		self.relops = {'<','>','<=','>=','==','!=','+','-','*','/','%'}
+	
+
+
+
+
+
+	def write(self, text):
+		'''Append the text passed in to the current line.'''
+		self.f.write(text)
+
+	def enter(self):
+		'''Create a new scope associated with the corresponding : and appropriate indentation.'''
+
 
