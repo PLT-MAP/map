64cb2b8842c61181f2a127f56fe15f22105e0868
Tommy Inouye <ti2181@columbia.edu>
2014-04-14 01:41:54 -0400

    added traverse tester but not sure if it works

--- traverseTester.py
+++ traverseTester.py
@@ -2,32 +2,31 @@ import sys
 import unittest
 from yacc import *
 from MAPlexer import *
+from traverse import *
 
 
 
-class TestYaccSyntax(unittest.TestCase):
+class TestTraverseSyntax(unittest.TestCase):
 	def setUp(self):
 		self.parser=yacc.yacc()
 		lexer = lex.lex()
 
 	def testhelloworld(self):
 		 test= "func main(Text hi, Numeric bye) { Text t = 'Hello, world'; bye = 2}"
-		 result=self.parser.parse(test,lexer=lexer,tracking=True)
-		 print result
+		 self.traverse(test)
 
 	def testprintfunc(self):
 		 test= "func main(Text hi, Numeric bye) {print(hi);}"
-		 result=self.parser.parse(test,lexer=lexer,tracking=True)
-		 print result
+		 self.traverse(test)
 
 	def testarithmetic(self):
 		 test=  "func main(Text hi, Text bye) { Numeric n = 1+2;}"
-		 result=self.parser.parse(test,lexer=lexer,tracking=True)
-		 print result
+		 self.traverse(test)
+
 	def testgraphfunc(self):
 		test = "func main(Text hi, Numeric hello, Path hereisApath, Node heresanode){ Text oneMore = 1; Text hello = 2; hello = oneMore + hello;}"
-		result=self.parser.parse(test,lexer=lexer,tracking=True)
-		print result
+		self.traverse(test)
+
 	def testgraphfunction(self):
 		test='''
 		func createTrip() {
@@ -48,8 +47,8 @@ class TestYaccSyntax(unittest.TestCase):
 		flights.addEdge(flight5, flight9, flight7, flight234, flight49);
 		return flights;}
 		'''
-		result=self.parser.parse(test,lexer=lexer,tracking=True)
-		print result
+		self.traverse(test)
+
 	def testgraphfunction1(self):
 		test='''
 		func printDestination(Node origin, Graph graph) {       
@@ -73,9 +72,7 @@ class TestYaccSyntax(unittest.TestCase):
 		printDestination(seattle, trip);
 		}
 		'''
-		result=self.parser.parse(test,lexer=lexer,tracking=True)
-		print result
-
+		self.traverse(test)
 
 
 
