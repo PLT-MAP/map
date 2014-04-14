3d1ee9742aaaa56d707dc715f60199f537db3bd2
Alex M <mereck@gmail.com>
2014-04-14 18:02:20 -0400

    nothing

--- yacc.py
+++ yacc.py
@@ -241,7 +241,7 @@ def p_funcname(t):
 	| EQUALSFUNC'''
 	t[0] = Node('','function_name',[t[1]])
 
-i = "func main(Text hi){hi = 'hello';}"
+#i = "func main(Text hi){hi = 'hello';}"
 #i = "func main(Text hi, Text bye) { Numeric n = 1+2;}"
 #i = "func main(Text hi, Numeric bye) {print(hi);}"
 #i = "func main(Text hi, Numeric bye) { Text t = 'Hello, world'; bye = 2}"
