f3a2612d79b08d2a588ce836c9656d7b30ad3cac
Alex M <mereck@gmail.com>
2014-04-16 09:40:17 -0400

    adding node

---
+++ node.py
@@ -0,0 +1,23 @@
+class Expr : pass
+
+class Node(Expr):
+	def __init__(self,name='',t='',children=None,leaf=None,token=None):
+		self.type = t
+
+		if children:
+			self.children = children
+		else:
+			self.children = []
+			
+		self.leaf = leaf
+		self.token = token
+		self.name = name
+	
+
+	def addChild(self,c):
+		self.children.append(c)
+
+	def __str__(self):
+		return "name:{1}  type:{0}".format(self.name,self.type)
+
+
