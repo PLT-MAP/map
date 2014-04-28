68d447ddcbfa5b1735b446afe6f945c4958e0f73
Alex M <mereck@gmail.com>
2014-04-28 16:02:22 -0400

    fixing node construction for lessthan

--- interpreter.py
+++ interpreter.py
@@ -9,22 +9,28 @@ def walkAst(t,state={}):
 
 	#check types in arithmetic operations
 
-	if state.get('processingAdditiveExpression') == 1:
-		print t
-	elif t.type == 'additive-expression':# or childTypeIs('additive-expression'):
-		print t
-		state['processingAdditiveExpression'] = 1
+	#if state.get('processingAdditiveExpression') == 1:
+	#	print t
+	#elif t.type == 'additive-expression':# or childTypeIs('additive-expression'):
+	#	print t
+	#	state['processingAdditiveExpression'] = 1
 
+	print t
 	
 	for n in t.children:
 		walkAst(n,state)
 	
-	state['processingAdditiveExpression'] = 0
+	 
+	#state['processingAdditiveExpression'] = 0
 
-i = "func main(Text hi,Numeric NumericArg, Graph GraphArg, Path PathArg, Node NodeArg) {Text hi = 'bye';}"
+#i = "func main(Text hi,Numeric NumericArg, Graph GraphArg, Path PathArg, Node NodeArg) {Text hi = 'bye';}"
+i = '''
+func main(Text hi) {if (5 < 7) {bye = 5;}}
+'''
 l = MAPlex()
 l.input = i
 l.tokenize(i)
 m = MAPparser(l,i)
-print draw_tree(m.ast)
+walkAst(m.ast)
+#print draw_tree(m.ast)
 
