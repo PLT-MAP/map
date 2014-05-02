5e17b0798dc6d4f353ec4a4b172faf390c21be58
altan92 <atan0092@gmail.com>
2014-05-01 22:53:31 -0400

    adding edge translations

--- test/sample1.py
+++ test/sample1.py
@@ -1,12 +1,13 @@
 import networkx as nx
 def main():	
-	g=nx.Graph()
+	g=nx.MultiDiGraph()
 	nj = "nj"
 	ny = "ny"
 	pa = "pa"
 	padict= {'temp':85,'humidity':'low'}
 	va = "va"
 	vadict= {'temp':87,'humidity':'high'}
+	print g.nodes()
 	
 if __name__== '__main__':
 	main()
--- test/sample3.py
+++ test/sample3.py
@@ -1,14 +1,26 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-G=nx.Graph()
-G.add_node(1)
-G.add_node("e")
-dict= {'cabo':,'cost':30}
-G.add_node( dict)
-G.add_edge(1,"e")
-G.add_edge("e", "cabo")
-print G.number_of_nodes()
-print G.nodes(data=True)
-print G.edges()
-nx.draw(G)
-plt.show()
+def main():
+        g=nx.MultiDiGraph()
+        nj = "nj"
+        ny = "ny"
+        pa = "pa"
+        padict= {'temp':85,'humidity':'low'}
+        va = "va"
+        vadict= {'temp':87,'humidity':'high'}
+	flight1= ["nj","ny"]
+	flight2 = ["ny", "pa"]
+	flight3= ["pa","va"]
+	flight3dict= {'cost':302, 'distance':4092}
+        g.add_node(nj)
+        g.add_node(ny)
+        g.add_nodes_from([(pa,padict),(va,vadict)])
+	g.add_edge(flight1[0],flight1[1])
+	g.add_edges_from([(flight2[0],flight2[1])])
+        print g.nodes(data=True)
+	print g.edges()
+	nx.draw(g)
+	plt.show()
+
+if __name__== '__main__':
+        main()
