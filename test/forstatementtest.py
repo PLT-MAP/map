565a45056a80693db2491c52fc3ca17153baeccd
Tommy Inouye <ti2181@columbia.edu>
2014-05-02 16:34:10 -0400

    For statements and if/elif/else statements working fine! tests are included for alll

--- test/forstatementtest.py
+++ test/forstatementtest.py
@@ -1,7 +1,7 @@
 import networkx
 import sys
 def main():
-    for i in range(0.0, 10.0, -2.0):
+    for i in range(0, 10):
         print i
 if __name__ == '__main__': 
 	main()
--- test/ifelsetest.py
+++ test/ifelsetest.py
@@ -2,6 +2,8 @@ import networkx
 import sys
 def main():
     if (10.0<7.0):
+        if (10.0<7.0):
+            print "hi"
         cost = 2.0
     else:
         print "success"
